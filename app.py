7941c915fe95c8a1efc7f357dc020def88de5910
Sraman <sramakoo@gmail.com>
2019-02-24 14:34:00 +0530

    course high level api

--- app.py
+++ app.py
@@ -26,6 +26,44 @@ def courses():
         count+=1
     return json.dumps(data)
 
+@app.route('/coursesize')
+def coursesize():
+    mycol = mydb["courses"]
+    count = 0
+    for course in mycol.find({},{'_id':0}):
+        count+=1
+    return str(count)
+
+@app.route('/coursetext')
+def coursetext():
+    index = int(request.args.get('index'))
+    mycol = mydb["courses"]
+    data = "<p>"
+    count = 0
+    for course in mycol.find({},{'_id':0}):
+        count+=1
+        if(count==index):
+            for subject in course['subjects']:
+                line = subject['subject']+' : '+subject['faculty']
+                data += line
+                data += '<br>'
+            data += ','.join(course['branch'])+' - '+','.join(course['standard'])
+            data += '<br>'
+    data += '</p>'
+    return data
+
+@app.route('/coursename')
+def coursename():
+    index = int(request.args.get('index'))
+    mycol = mydb["courses"]
+    data = ""
+    count = 0
+    for course in mycol.find({},{'_id':0}):
+        count+=1
+        if(count==index):
+            data += course['name']
+    return data
+
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
 	CORS(app, resources=r'/*')
